6cbec6396b78c45a056b9c826b9eaccfba55b41a
sara <sara.denoual@stg.fr>
2020-04-02 15:28:33 +0200

    Ajoutez des fichiers projet.

---
+++ Xamarin-Module9-TP06/Xamarin-Module9-TP06/ListTweets.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin_Module9_TP06.Entities;
+using Xamarin_Module9_TP06.Services;
+
+namespace Xamarin_Module9_TP06
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListTweets : ContentPage
+    {
+        public ListTweets()
+        {
+            InitializeComponent();            
+            listTweets.ItemsSource = new TwitterServiceImpl().Tweets;
+        }
+
+        
+    }
+}
